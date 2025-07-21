8334dfb90afab9e978943e8dca9a83dbb1eeda8b
drk-m-s <john.jianghan@gmail.com>
2025-07-21 14:51:15 +0800

    [fix] using code template from 3B

--- scripts_simulation/qwen2.5_VL_7B.py
+++ scripts_simulation/qwen2.5_VL_7B.py
@@ -1,75 +1,120 @@
-import argparse
-import sys
-import time
 import torch
+import transformers
+import sys
+import argparse
+from PIL import Image
+import requests
+from io import BytesIO
+
+your_token="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"
+
+# Qwen2.5-VL-3B-Instruct
+from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from qwen_vl_utils import process_vision_info
+
 # Add your local src directory to Python path
 sys.path.insert(0, '/home/han.jiang/simulator/src')
 
-import transformers
-from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
-from qwen_vl_utils import process_vision_info
-
 import upmem_llm_framework.pytorch_upmem_layers as upmem_layers
-# upmem_layers.profiler_init()
+upmem_layers.profiler_init()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--device", default="unknown")
 parser.add_argument("--in-tokens", default=64, type=int)
-parser.add_argument("--out-tokens", default=128, type=int)
+parser.add_argument("--out-tokens", default=30, type=int)
 parser.add_argument("--bs", default=1, type=int)
 options, _ = parser.parse_known_args()
 
-print ("Simulating with device...", options.device)
-print ("in:", options.in_tokens, "out:", options.out_tokens)
-
-your_token="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"
-
-model     = Qwen2_5_VLForConditionalGeneration.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", token=your_token,torch_dtype="auto", device_map="auto")
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", token=your_token)
-
 layer_mapping = {
-        "LlamaRMSNorm"    : options.device,
-        "q_proj"	  : options.device,
-        "k_proj"	  : options.device,
-        "rotatory_emb"    : options.device,
-        "v_proj"          : options.device,
-        "o_proj"          : options.device,
-        "output_layernorm": options.device,
+        "proj"            : options.device,
+        "rotary_pos_emb"  : options.device,
+        "norm1"           : options.device,
+        "norm2"           : options.device,
+        "qkv"             : options.device,
         "gate_proj"       : options.device,
         "up_proj"         : options.device,
         "down_proj"       : options.device,
+        "act_fn"          : options.device,
+        "q_proj"	      : options.device,
+        "k_proj"		  : options.device,
+        "rotatory_emb"    : options.device,
+        "v_proj"          : options.device,
+        "o_proj"		  : options.device,
+        "input_layernorm" : options.device,
+        "post_attention_layernorm": options.device,
         "norm"            : options.device,
         "lm_head"         : options.device
 }
-layer_attn_ctxt = "q_proj"
-
-print (model)
-
-# print ("Batch 1")
-# prompt = "placeholder"
-# prompt_batch = [prompt] * options.bs
-# input_ids = tokenizer(prompt_batch, return_tensors="pt", return_token_type_ids=False)
-# input_ids["input_ids"] = torch.randint(100, [options.bs, options.in_tokens])
-# input_ids["attention_mask"] = torch.ones([options.bs, options.in_tokens], dtype=torch.int)
-# print (input_ids.data["input_ids"][0].shape)
 
 
-# model.eval()
+# default: Load the model on the available device(s)
+model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+    "Qwen/Qwen2.5-VL-7B-Instruct", torch_dtype="auto", device_map="auto"
+)
 
-# upmem_layers.profiler_start(layer_mapping, layer_attn_ctxt = layer_attn_ctxt)
-# #start = time.time_ns()
-# gen_tokens = model.generate(**input_ids,
-#                             do_sample=True,
-#                             temperature=0.9,
-# 							min_length=options.out_tokens,
-#                             max_length=options.out_tokens+100)
-# #print ( (time.time_ns() - start)/1e6)
-# upmem_layers.profiler_end  ()
+# default processor
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct",use_fast=False)
 
-# gen_text = tokenizer.batch_decode(gen_tokens)
-# print (gen_text)
+# The default range for the number of visual tokens per image in the model is 4-16384.
+# You can set min_pixels and max_pixels according to your needs, such as a token range of 256-1280, to balance performance and cost.
+# min_pixels = 256*28*28
+# max_pixels = 1280*28*28
+# processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
 
-# sys.exit()
 
-#------------------------------------------------------------------------#
+# # Download and resize the image
+# img_url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg"
+# response = requests.get(img_url)
+# img = Image.open(BytesIO(response.content)).convert("RGB")
+# img = img.resize((468, 364))  # width, height
 
+messages = [
+    {
+        "role": "user",
+        "content": [
+            {
+                "type": "image",
+                "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
+            },
+            {"type": "text", "text": "Describe this image like I am a 4 year old and don't know much about anything."},
+        ],
+    }
+]
+
+upmem_layers.profiler_start(layer_mapping, batch_size=options.bs)
+
+# Preparation for inference
+text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
+)
+
+image_inputs, video_inputs = process_vision_info(messages)
+
+inputs = processor(
+    text=[text],
+    images=image_inputs,
+    videos=video_inputs,
+    padding=True,
+    return_tensors="pt",
+)
+inputs = inputs.to(model.device)
+
+print("Total input tokens:", inputs.input_ids.shape[1])
+
+# print ("Simulating with device...", options.device)
+print ("in:", inputs.input_ids.shape[1], "out:", options.out_tokens)
+
+
+# Inference: Generation of the output
+generated_ids = model.generate(**inputs, min_new_tokens=options.out_tokens, max_new_tokens=options.out_tokens+1)
+generated_ids_trimmed = [
+    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+]
+
+upmem_layers.profiler_end()
+
+output_text = processor.batch_decode(
+    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+)
+print(output_text)
